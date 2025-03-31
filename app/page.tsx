84e848f942e56aa89caaf951c62c00df6cfedbc6
kame_mac <kametyaso@gmail.com>
2025-03-31 19:28:46 +0900

    axiosの導入および動作検証

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,33 @@
-"use client";
-import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const DEFAULT_API_CONFIG = {
+  baseURL: "http://localhost:3000/api/v1",
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+};
+
+const api = axios.create(DEFAULT_API_CONFIG);
+
+const response = await axios.get("https://jsonplaceholder.typicode.com/users");
 
 export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-center p-24">main</main>;
+  // console.log("api headers", api.defaults.headers);
+  console.log(response.data);
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-center p-24">
+        main
+        {response.data.map((user) => (
+          <div key={user.id}>
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+          </div>
+        ))}
+      </main>
+      ;
+    </>
+  );
 }
